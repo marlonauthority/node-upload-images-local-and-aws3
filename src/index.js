581f62e762e54c48875b6f3db2c7d4c3ecc8834a
marlondck <15050029+marlondck@users.noreply.github.com>
2019-03-16 16:42:49 -0400

    envio de imagens locais e aws s3

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true
+});
+
+//define a config para o express conseguir lidar com requisicoes rest
+app.use(express.json());
+//define para o express lidar com req urlenconded
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
+app.use(require("./routes"));
+
+app.listen(3000);
